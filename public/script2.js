a63a3b34291b32473bef88356d95235ddb5e7132
Jordan Zinger <jzinger18@gmail.com>
2017-07-29 13:38:59 -0400

    moviedb generate posters

--- public/script2.js
+++ public/script2.js
@@ -37,4 +37,60 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+
+var queryUrl = "https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2017-07-01&api_key=72f30fe7c45d9a6791b64485aa95f563";
+
+
+//Ajax get request
+$.ajax({
+  url: queryUrl,
+  method: 'GET'
+})
+  .done(function(response){
+
+    console.log(response);
+    var movie_list = response.results
+    var limit = 4
+    console.log(movie_list)
+    for(var i = 0; i< limit; i++) {
+      //hold all content generated for movie poster
+     holder = $("<div class='col-md-3'>")
+     //create a thumbnail
+   movieDiv = $("<div class='thumbnail'>")
+   //holds gif image
+   movieImg = $('<img class="gif" data-state="still img-responsive">');
+
+   imgUrl = "http://image.tmdb.org/t/p/w300/"
+   //add gif
+   imgPoster = imgUrl + movie_list[i].poster_path;
+
+
+
+   movieImg.attr("src", imgPoster)
+
+   movieCaptionDiv = $("<div class='caption'>")
+
+   movieCaptionP = $('<p>')
+
+   movieCaptionP.text(movie_list[i].overview)
+
+
+//Append gifImage to Gif div
+movieDiv.append(movieImg)
+holder.append(movieDiv)
+movieCaptionDiv.append(movieCaptionP)
+holder.append(movieCaptionDiv)
+
+//Append gifDiv to HTML
+$('#movie-sect').append(holder)
+
+}
+    })
+
+
+
+
+
+
+
 })
